bfac8271c92757ac924a8fcd3b6faccb5ae7baf5
lzaglewski <l.zaglewski@gmail.com>
2016-05-23 16:24:42 +0200

    Prace nad koszykiem

--- walidacja.js
+++ walidacja.js
@@ -2,74 +2,107 @@
  * Created by Lukasz on 18.03.16.
  */
 
+    var editting=false;
 
 var product=new Object();
 //var productTable=new Array();
 
-var productTable=[
-    {
-        name:'dupa',
-        code:'83-23',
-        netto:12,
-        vat:22,
-        brutto:12.64,
-        category:'kategoria 1',
-        option:'1',
-        rate:1
+var productTable=[];
 
-    },
+function delRow(id){
 
-    {
-        name:'zzzz',
-        code:'83-23',
-        netto:12,
-        vat:22,
-        brutto:77,
-        category:'kategoria 1',
-        option:'1',
-        rate:2
+    productTable.splice(id,1);
+    drawTable(productTable);
 
-    },
-    {
-        name:'gfgfgf',
-        code:'83-23',
-        netto:12,
-        vat:22,
-        brutto:98,
-        category:'kategoria 1',
-        option:'1',
-        rate:4
+}
 
-    },
 
-    {
-        name:'dfdfd',
-        code:'83-23',
-        netto:12,
-        vat:22,
-        brutto:7798,
-        category:'kategoria 1',
-        option:'1',
-        rate:5
+function setCookie()
+{
+
+    var objToCokie= productTable;
+    var stringObj=JSON.stringify(objToCokie);
+
+    $.cookie('1',stringObj);
+
+}
+
+
+
+function ReadCookie() {
+
+    if ($.cookie('1')) {
+
+
+    var obj = JSON.parse($.cookie('1'));
+
+    productTable = obj;
+    drawTable(productTable);
+}
+
+}
+
+
+
+function editRow(id){
+
+    editting=true;
+
+
+
+    $('#modalForm').modal();
+    $('#formAddProduct').find('[id="name"]').val(productTable[id].name);
+    $('#formAddProduct').find('[id="code"]').val(productTable[id].code);
+    $('#formAddProduct').find('[id="netto"]').val(productTable[id].netto);
+    $('#formAddProduct').find('[id="vat"]').val(productTable[id].vat);
+    $('#formAddProduct').find('[id="brutto"]').val(productTable[id].brutto);
+
+    document.getElementById("sel1").value=(productTable[id].category);
+
+    var i;
+    for(i=0;i<5;i++){
+
+        if(productTable[id].option[i]!=undefined)
+        {
+            if(productTable[id].option[i]==1) document.getElementById("c1").checked=true;
+            if(productTable[id].option[i]==2) document.getElementById("c2").checked=true;
+            if(productTable[id].option[i]==3) document.getElementById("c3").checked=true;
+            if(productTable[id].option[i]==4) document.getElementById("c4").checked=true;
+            if(productTable[id].option[i]==5) document.getElementById("c5").checked=true;
+
+        }
 
     }
 
+    editting=false;
 
-];
-
-
-
+}
 function createRow(){
-    
+
 
     var temp=jQuery.extend({}, product);
     productTable.push(temp);
 
-    drawTable();
+    drawTable(productTable);
 }
 
 
-function drawTable(){
+function drawKosz(){
+
+    $("#koszykTable").html(" ");
+
+    for(var i in productTable) {
+        $("#koszykTable").append(
+            '<tr scope="row">'+
+            '<th>' + productTable[i].name + '</th>'+
+            '<td>' + productTable[i].brutto + '</td>'+
+
+
+            '</tr>');
+    }
+
+}
+function drawTable(productTable){
 
     $("#tableBody").html(" ");
 
@@ -85,17 +118,24 @@ function drawTable(){
             '<td>' + productTable[i].category + '</td>'+
             '<td>' + productTable[i].option + '</td>'+
             '<td>' + productTable[i].rate + '</td>'+
+            '<td><button onclick="delRow(this.id)" class="del" id="'+i+'">usuń</button></td>'+
+            '<td><button onclick="editRow(this.id)" class="del" id="'+i+'">edycja</button></td>'
 
-
++
             '</tr>');
     }
+
+    setCookie();
 }
 
 ///buttony
 $(document).ready(function(){
     ////buttony sort
 
-    drawTable();
+   // drawTable(productTable);
+    ReadCookie();
+
+
 
     $("#Nsort").click(function() {
 
@@ -108,7 +148,7 @@ $(document).ready(function(){
                 return x < y ? -1 : x > y ? 1 : 0;
             });
             productTable = byName;
-            drawTable();
+            drawTable(productTable);
             document.getElementById("Nsort").value="ON";
         } else if(document.getElementById("Nsort").value=="ON"){
 
@@ -119,7 +159,7 @@ $(document).ready(function(){
                 return x < y ? 1 : x > y ? -1 : 0;
             });
             productTable = byName;
-            drawTable();
+            drawTable(productTable);
             document.getElementById("Nsort").value="OFF";
         }
 
@@ -138,7 +178,7 @@ $(document).ready(function(){
                 return x < y ? -1 : x > y ? 1 : 0;
             });
             productTable = byName;
-            drawTable();
+            drawTable(productTable);
             document.getElementById("Bsort").value="ON";
         } else if(document.getElementById("Bsort").value=="ON"){
 
@@ -149,7 +189,7 @@ $(document).ready(function(){
                 return x < y ? 1 : x > y ? -1 : 0;
             });
             productTable = byName;
-            drawTable();
+            drawTable(productTable);
             document.getElementById("Bsort").value="OFF";
         }
 
@@ -169,7 +209,7 @@ $(document).ready(function(){
                 return x < y ? -1 : x > y ? 1 : 0;
             });
             productTable = byName;
-            drawTable();
+            drawTable(productTable);
             document.getElementById("Rsort").value="ON";
         } else if(document.getElementById("Rsort").value=="ON"){
 
@@ -180,15 +220,28 @@ $(document).ready(function(){
                 return x < y ? 1 : x > y ? -1 : 0;
             });
             productTable = byName;
-            drawTable();
+            drawTable(productTable);
             document.getElementById("Rsort").value="OFF";
         }
 
     });
     /////
 
+    $('#cookButton').click(function(){
+
+    });
+
+    $('#del').click(function(){
+
+        alert($('.del').val);
+
+
+    });
+
 
     $("#delButton").click(function(){
+
+
 
         var id=new Array()
 
@@ -207,7 +260,39 @@ $(document).ready(function(){
 
         }
 
-        drawTable();
+        drawTable(productTable);
+
+    });
+
+
+
+
+    $("#koszButton").click(function(){
+
+
+
+        var id=new Array()
+
+        $('input:checked ').each(function(){
+
+
+            id.push(parseInt(this.id));
+
+        });
+
+        var length=id.length;
+
+        for(var i=0;i<length;i++)
+        {
+           // productTable.splice(id.pop(),1);
+            alert(id);
+
+        }
+
+
+
+       drawKosz();
+        $('#modalKosz').modal();
 
     });
 
@@ -267,191 +352,187 @@ $(document).ready(function() {
     /////submit button
 
     $("#submit").click(function(){
-        ///name
-        var name=$("#name").val();
-        if(name=="" ||  name.length>10) {
-            $("#name").css('border-color', 'red');
-            $("#alertName").css('display','block');
-            product.name=undefined;
 
+        if(editting!=true) {
+            ///name
+            var name = $("#name").val();
+            if (name == "" || name.length > 10) {
+                $("#name").css('border-color', 'red');
+                $("#alertName").css('display', 'block');
+                product.name = undefined;
 
-        }else{
-            $("#name").css('border-color', 'green');
-            $("#alertName").css('display','none');
 
-            product.name=name;
-        }
-        ///name
+            } else {
+                $("#name").css('border-color', 'green');
+                $("#alertName").css('display', 'none');
 
+                product.name = name;
+            }
+            ///name
 
-        ///code
 
-        var code=$("#code").val();
-        var kod_wzorzec =  /^[a-zA-Z0-9]{2}-[a-zA-Z0-9]{2}$/;
+            ///code
 
-        if(kod_wzorzec.test(code) && code.length>=5){
-            $("#code").css('border-color', 'green');
-            $("#alertCode").css('display','none')
+            var code = $("#code").val();
+            var kod_wzorzec = /^[a-zA-Z0-9]{2}-[a-zA-Z0-9]{2}$/;
 
-            product.code=code;
-        }
-        else{
-            $("#code").css('border-color', 'red');
-            $("#alertCode").css('display','block');
+            if (kod_wzorzec.test(code) && code.length >= 5) {
+                $("#code").css('border-color', 'green');
+                $("#alertCode").css('display', 'none')
 
-            product.code=undefined;
-        }
+                product.code = code;
+            }
+            else {
+                $("#code").css('border-color', 'red');
+                $("#alertCode").css('display', 'block');
 
+                product.code = undefined;
+            }
 
-        //code
 
+            //code
 
-        ///netto
 
-        netto=$("#netto").val();
+            ///netto
 
-        if($.isNumeric(netto) && netto.length>0){
-            $("#netto").css('border-color', 'green');
-            $("#alertNetto").css('display','none');
-            nettoOk=true;
-           var zeroes=addZeroes(netto)
-            $("#netto").val(zeroes);
+            netto = $("#netto").val();
 
-            product.netto=zeroes;
+            if ($.isNumeric(netto) && netto.length > 0) {
+                $("#netto").css('border-color', 'green');
+                $("#alertNetto").css('display', 'none');
+                nettoOk = true;
+                var zeroes = addZeroes(netto)
+                $("#netto").val(zeroes);
 
-            bruttoCalc();
+                product.netto = zeroes;
 
-        }
-        else{
-            $("#netto").css('border-color', 'red');
-            $("#alertNetto").css('display','block');
-            nettoOk=false;
-            product.netto=undefined;
-            bruttoCalc();
-        }
-
-        ///netto
-
-        ///
-
-
-        ///
-
-        ///vat
-
-        vat=$("#vat").val();
-
-        if($.isNumeric(vat) && vat.length>0){
-            $("#vat").css('border-color', 'green');
-            $("#alertVat").css('display','none');
-            vatOk=true;
-
-            product.vat=vat;
-
-            bruttoCalc();
-
-
-        }
-        else{
-            $("#vat").css('border-color', 'red');
-            $("#alertVat").css('display','block');
-            vatOk=false;
-
-            product.vat=undefined;
-
-            bruttoCalc();
-        }
-        ///vat
-        ////select kategoria
-
-        var sel=$("#sel1").val();
-        product.category=sel;
-
-        /////checkboxes opcja
-
-        var checkboxlist=[];
-
-        $("input[type=checkbox]:checked").each(function() {
-
-
-            checkboxlist.push($(this).val() );
-
-        });
-
-        if(checkboxlist.length>=2)
-        {
-            $("#alertOpcja").css('display','none');
-
-            product.option=checkboxlist;
-
-        }
-        else{
-
-            $("#alertOpcja").css('display','block');
-
-            product.option=undefined;
-
-        }
-
-        /////checkboxes
-
-
-        ///radio
-
-        product.rate=undefined;
-
-
-        $("input[type=radio]:checked").each(function() {
-
-
-           product.rate=$(this).val();
-
-
-        });
-
-
-
-
-        ////radio
-
-
-
-        /////sprawdza czy w obiekcie jest jakis undefined
-
-        var value=[];
-        var formOk=false;
-
-        for(var key in product) {
-
-
-            value.push(product[key]);
-            if(product[key]===undefined)
-            {
-                formOk=false;
-                break;
+                bruttoCalc();
 
             }
-            else{
-                formOk=true;
+            else {
+                $("#netto").css('border-color', 'red');
+                $("#alertNetto").css('display', 'block');
+                nettoOk = false;
+                product.netto = undefined;
+                bruttoCalc();
             }
+
+            ///netto
+
+            ///
+
+
+            ///
+
+            ///vat
+
+            vat = $("#vat").val();
+
+            if ($.isNumeric(vat) && vat.length > 0) {
+                $("#vat").css('border-color', 'green');
+                $("#alertVat").css('display', 'none');
+                vatOk = true;
+
+                product.vat = vat;
+
+                bruttoCalc();
+
+
+            }
+            else {
+                $("#vat").css('border-color', 'red');
+                $("#alertVat").css('display', 'block');
+                vatOk = false;
+
+                product.vat = undefined;
+
+                bruttoCalc();
+            }
+            ///vat
+            ////select kategoria
+
+            var sel = $("#sel1").val();
+            product.category = sel;
+
+            /////checkboxes opcja
+
+            var checkboxlist = [];
+
+            $("input[type=checkbox]:checked").each(function () {
+
+
+                checkboxlist.push($(this).val());
+
+            });
+
+            if (checkboxlist.length >= 2) {
+                $("#alertOpcja").css('display', 'none');
+
+                product.option = checkboxlist;
+
+            }
+            else {
+
+                $("#alertOpcja").css('display', 'block');
+
+                product.option = undefined;
+
+            }
+
+            /////checkboxes
+
+
+            ///radio
+
+            product.rate = undefined;
+
+
+            $("input[type=radio]:checked").each(function () {
+
+
+                product.rate = $(this).val();
+
+
+            });
+
+
+            ////radio
+
+
+            /////sprawdza czy w obiekcie jest jakis undefined
+
+            var value = [];
+            var formOk = false;
+
+            for (var key in product) {
+
+
+                value.push(product[key]);
+                if (product[key] === undefined) {
+                    formOk = false;
+                    break;
+
+                }
+                else {
+                    formOk = true;
+                }
+            }
+
+            if (formOk === true) {
+                alert("formularzok");
+
+                createRow(product);
+                // document.getElementById("formAddProduct").reset();
+                $("#modalForm").modal("hide");
+
+
+            }
+
+
+            /////sprawdza czy w obiekcie jest jakis undefined end
+
+
         }
-
-        if(formOk===true){
-            alert("formularzok");
-
-            createRow(product);
-           // document.getElementById("formAddProduct").reset();
-            $("#modalForm").modal("hide");
-
-
-        }
-
-
-        /////sprawdza czy w obiekcie jest jakis undefined end
-
-
-
-
     });
 
 
